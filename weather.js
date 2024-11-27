774c2fea8e98c58edd6cff159a7c8234ca55fe1e
tomatlampa <korolev94sergey@gmail.com>
2024-11-27 19:09:45 +0300

    Update weather.js

--- weather.js
+++ weather.js
@@ -31,7 +31,7 @@
             var condition = data2.condition.text;// Обстановка
 				console.log("Погода", "Обстановка: " + condition)
 
-            $('#weather-temp').text(temp + '°').fontsize(4);
+            $('#weather-temp').text(temp + '°');
             $('#weather-condition').text(condition).toggleClass('long-text', condition.length > 10);
         }
 
